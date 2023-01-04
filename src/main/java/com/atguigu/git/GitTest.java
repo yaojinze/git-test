7d1afa6134343d55d0f375e2c968129e8bc6f888
yaojinze <98305142+yaojinze@users.noreply.github.com>
2023-01-04 23:39:40 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hot-fix test2!");
         System.out.println("push test!");
         System.out.println("push test2!");
+        System.out.println("pull test!");
+
     }
 }
