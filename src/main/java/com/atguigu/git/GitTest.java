fb212fab8a8e0f94ed54bd232a999e97b7ffe275
yjznzsa <yjznzsa@666.com>
2023-01-06 00:16:09 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("push test2!");
         System.out.println("pull test!");
         System.out.println("pull test3!");
+        System.out.println("push test3!");
 
     }
 }
