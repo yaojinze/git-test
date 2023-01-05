62134048035c878cd835ea75bb36a9f7766b2567
姚锦泽 <2411624827@qq.com>
2023-01-05 16:20:17 +0000

    git pull
    Signed-off-by: 姚锦泽 <2411624827@qq.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("pull test!");
         System.out.println("pull test3!");
         System.out.println("push test3!");
+        System.out.println("push test4!");
 
     }
 }
